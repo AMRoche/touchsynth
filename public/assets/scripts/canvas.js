a94d41113fb4864016cce4848d85ee50e94460c1
AMRoche <alexandermichaelroche@gmail.com>
2014-12-27 09:21:41 +0000

    Feat ( Canvas.js ) : Changed processing code to some sensible JS.

--- public/assets/scripts/canvas.js
+++ public/assets/scripts/canvas.js
@@ -1,74 +1,63 @@
-
-  int yStart = 0;
+var yStart = 0;
+var hexSize = 5;
+var hexSpace = 5;
   
-  int hexSize = 5;
-  
-  int hexSpace = 5;
-  
-void setup()
+function setupCanvas()
 {  
-  size (300, 300)
+  //size(300, 300)
   
   if(hexSize <= 0) {
     hexSize = 1;
   }
+  
+  if(hexSpace <= 0) {
+  		hexSpace = 1;
+  }
 }
 
-void draw () 
+function drawHexes() 
 {
-yStart = origYStart;
-int hexWidth = hexSize*2;
-float hexHeight = sqrt(3)*hexSize;
+	yStart = origYStart;
+	var hexWidth = hexSize*2;
+	var hexHeight = sqrt(3)*hexSize;
 
- for(int j = 0; j < (floor((y) / (hexHeight/2)) ); j++ )
-  {
-    
-for(int i = 0; i < (floor( (x) /(hexWidth*1.5)  )  ); i++ )
-{
-  if( j % 2 != 0 && i == 0)
-  {
-    xStart += (hexWidth*0.75+ ( (hexSpace*(5/3))));
-  }
-  
-  if(i > 0)
-  {
-    xStart += hexSize;
-  }
-  
-  if (xStart > (x-hexWidth) || yStart > (y-hexHeight))
-  {
-    break;
-  }
-
-  fill(random(255), random(255), random(255));
-    beginShape();
-  
-  vertex( xStart+(hexSize/2), yStart );
-  vertex( xStart+ (hexSize*1.5), yStart );
-  
-  vertex ( xStart + (hexSize * 2 ), yStart + hexHeight /2);
-  
-  vertex ( xStart + (hexSize * 1.5 ), yStart + hexHeight );
-  vertex ( xStart + (hexSize * 0.5 ), yStart +hexHeight );
-
-  vertex ( xStart, yStart + (hexHeight/2) );
-  
-  endShape(CLOSE);
-  xStart += ( hexWidth + (hexSpace*(5/3)*2) );
-  
-  
-  }
-  
-  yStart += (hexHeight/2)+hexSpace/2;
-  
-  xStart = origXStart;
-
-//  
+	for(var j = 0; j < (~~((y) / (hexHeight/2)) ); j++ )
+	{    
+			for(var i = 0; i < (~~( (x) /(hexWidth*1.5)  )  ); i++ )
+			{
+			  if( j % 2 != 0 && i == 0)
+			  {
+			    xStart += (hexWidth*0.75+ ( (hexSpace*(5/3))));
+			  }
+			  
+			  if(i > 0)
+			  {
+			    xStart += hexSize;
+			  }
+			  
+			  if (xStart > (x-hexWidth) || yStart > (y-hexHeight))
+			  {
+			    break;
+			  }
+	
+			  	drawHex(xStart, yStart, hexSize);	  
+					xStart += ( hexWidth + (hexSpace*(5/3)*2) );
+			 }	
+			   
+			  yStart += (hexHeight/2)+hexSpace/2;  
+			  xStart = origXStart;
+		}
 }
 
-stroke(255,0,0);
-line(200,0,200,200);
-line(0,200,200,200);
-
-stroke(0);
+function drawHex(xStart, yStart, hexSize)
+{
+	  fill(random(255), random(255), random(255));
+	  beginShape();
+	  vertex( xStart+(hexSize/2), yStart );
+	  vertex( xStart+ (hexSize*1.5), yStart );
+	  vertex ( xStart + (hexSize * 2 ), yStart + hexHeight /2);
+	  vertex ( xStart + (hexSize * 1.5 ), yStart + hexHeight );
+	  vertex ( xStart + (hexSize * 0.5 ), yStart +hexHeight );
+	  vertex ( xStart, yStart + (hexHeight/2) );
+	  endShape(CLOSE);
 }
